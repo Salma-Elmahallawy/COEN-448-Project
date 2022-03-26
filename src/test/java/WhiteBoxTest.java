fecd721946a8dd513d6648e9e1ad438553302651
Salma-Elmahallawy <salma.mahallawy@gmail.com>
2022-03-26 13:36:59 -0400

    Commented test cases

--- src/test/java/WhiteBoxTest.java
+++ src/test/java/WhiteBoxTest.java
@@ -85,66 +85,89 @@ class WhiteBoxTest {
 
     @Test
     public void testCommandInput() {
+
         ByteArrayOutputStream outputStreamCaptor_message = new ByteArrayOutputStream();
         Main.initialize(10);
         int[][] array = new int[10][10];
         Main.robot.setPenUp(false);
+
+        //t1
         Main.CommandInput("U", 10, array);
         Assertions.assertTrue(Main.robot.isPenUp());
+
+        //t2
         Main.CommandInput("D", 10, array);
         Assertions.assertFalse(Main.robot.isPenUp());
+
+        //t3
         Main.CommandInput("R", 10, array);
         Assertions.assertEquals(1, Main.robot.getDirection());
+
+        //t4
         Main.CommandInput("L", 10, array);
         Assertions.assertEquals(0, Main.robot.getDirection());
         System.setOut(new PrintStream(outputStreamCaptor_message));
+
+        //t5
         Main.CommandInput("P", 10, array);
         Assertions.assertEquals("---+---+---+---+---+---+---+---+---+---+---+\r\n9  |   |   |   |   |   |   |   |   |   |   |\n---+---+---+---+---+---+---+---+---+---+---+\r\n8  |   |   |   |   |   |   |   |   |   |   |\n---+---+---+---+---+---+---+---+---+---+---+\r\n7  |   |   |   |   |   |   |   |   |   |   |\n---+---+---+---+---+---+---+---+---+---+---+\r\n6  |   |   |   |   |   |   |   |   |   |   |\n---+---+---+---+---+---+---+---+---+---+---+\r\n5  |   |   |   |   |   |   |   |   |   |   |\n---+---+---+---+---+---+---+---+---+---+---+\r\n4  |   |   |   |   |   |   |   |   |   |   |\n---+---+---+---+---+---+---+---+---+---+---+\r\n3  |   |   |   |   |   |   |   |   |   |   |\n---+---+---+---+---+---+---+---+---+---+---+\r\n2  |   |   |   |   |   |   |   |   |   |   |\n---+---+---+---+---+---+---+---+---+---+---+\r\n1  |   |   |   |   |   |   |   |   |   |   |\n---+---+---+---+---+---+---+---+---+---+---+\r\n0  |   |   |   |   |   |   |   |   |   |   |\n---+---+---+---+---+---+---+---+---+---+---+\r\n     0   1   2   3   4   5   6   7   8   9   \r\n", outputStreamCaptor_message.toString());
         outputStreamCaptor_message.reset();
+
+        //t6
         Main.CommandInput("C", 10, array);
         Assertions.assertEquals("Current Position: (0,0)\r\nPen Position: Down\r\nPen Direction: North\r\n", outputStreamCaptor_message.toString());
         outputStreamCaptor_message.reset();
+
+        //t7
         Main.CommandInput("Q", 10, array);
         Assertions.assertEquals("Exiting Program\r\n", outputStreamCaptor_message.toString());
         outputStreamCaptor_message.reset();
+
+        //t8
         Main.CommandInput("", 10, array);
         Assertions.assertEquals("Wrong Command Input\r\n", outputStreamCaptor_message.toString());
         outputStreamCaptor_message.reset();
     }
 
     @Test
-    public void testMoveForward() {
+    public void testMoveForward()
+    {
         ByteArrayOutputStream outputStreamCaptor_message = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStreamCaptor_message));
+
         int numOfSpaces = 5;
         int gridSize = 10;
-        int[][] grid = new int[gridSize][gridSize];
+        int[][] grid = new int [gridSize][gridSize];
+
+        //t1
+        //test case: pen up facing north move within border
         Main.initialize(10);
-        Main.setGrid(new int[gridSize][gridSize]);
+        Main.setGrid(new int [gridSize][gridSize]);
         Main.robot.setDirection(0);
         Main.robot.setxPosition(0);
         outputStreamCaptor_message.reset();
         Main.robot.setyPosition(0);
         Main.robot.setPenUp(true);
+
         Main.moveForward(numOfSpaces, gridSize, Main.getGrid());
+
         Assertions.assertEquals(0, Main.robot.getxPosition());
         Assertions.assertEquals(5, Main.robot.getyPosition());
         Assertions.assertTrue(Main.robot.isPenUp());
-        int[][] array = new int[10][10];
-        Main.printGrid(Main.getGrid(), 5);
 
-        int i;
-        int j;
-        for(i = 0; i <= Main.getGrid().length - 1; ++i) {
-            for(j = 0; j <= Main.getGrid().length - 1; ++j) {
-                Assertions.assertEquals(array[i][j], Main.getGrid()[i][j]);
+        int[][] array = new int[10][10];
+        Main.printGrid(Main.getGrid(),5);
+        for(int i = 0; i <= Main.getGrid().length - 1; i++){
+            for(int j = 0; j <= Main.getGrid().length - 1; j++){
+                Assertions.assertEquals(array[i][j],  Main.getGrid()[i][j]);
             }
         }
-
         outputStreamCaptor_message.reset();
         Assertions.assertEquals("", outputStreamCaptor_message.toString().trim());
+
+        //test case: pen down facing north move inside border
         Main.initialize(10);
-        Main.setGrid(new int[gridSize][gridSize]);
+        Main.setGrid(new int [gridSize][gridSize]);
         Main.robot.setxPosition(0);
         outputStreamCaptor_message.reset();
         Main.robot.setyPosition(0);
@@ -154,21 +177,20 @@ class WhiteBoxTest {
         Assertions.assertEquals(5, Main.robot.getyPosition());
         Assertions.assertFalse(Main.robot.isPenUp());
         array = new int[10][10];
-
-        for(i = 5; i >= 0; --i) {
-            array[0][i] = 1;
+        for(int i = 5; i >=0; i--) {
+            array[0][i]=1;
         }
-
-        Main.printGrid(Main.getGrid(), 10);
-
-        for(i = 0; i <= Main.getGrid().length - 1; ++i) {
-            for(j = 0; j <= Main.getGrid().length - 1; ++j) {
-                Assertions.assertEquals(array[i][j], Main.getGrid()[i][j]);
+        Main.printGrid(Main.getGrid(),10);
+        for(int i = 0; i <= Main.getGrid().length - 1; i++){
+            for(int j = 0; j <= Main.getGrid().length - 1; j++){
+                Assertions.assertEquals(array[i][j],  Main.getGrid()[i][j]);
             }
         }
-
         outputStreamCaptor_message.reset();
         Assertions.assertEquals("", outputStreamCaptor_message.toString().trim());
+
+        //t2
+        //test case: pen up facing north move outside border
         Main.robot.setxPosition(0);
         outputStreamCaptor_message.reset();
         Main.robot.setyPosition(9);
@@ -178,8 +200,9 @@ class WhiteBoxTest {
         Assertions.assertEquals(9, Main.robot.getyPosition());
         Assertions.assertTrue(Main.robot.isPenUp());
         Assertions.assertEquals("Invalid move (out of bound)", outputStreamCaptor_message.toString().trim());
+
         Main.initialize(10);
-        Main.setGrid(new int[gridSize][gridSize]);
+        Main.setGrid(new int [gridSize][gridSize]);
         Main.robot.setxPosition(0);
         outputStreamCaptor_message.reset();
         Main.robot.setyPosition(9);
@@ -189,8 +212,11 @@ class WhiteBoxTest {
         Assertions.assertEquals(9, Main.robot.getyPosition());
         Assertions.assertFalse(Main.robot.isPenUp());
         Assertions.assertEquals("Invalid move (out of bound)\r\n", outputStreamCaptor_message.toString());
+
+        //t3
+        //test case: pen down facing east move inside border
         Main.initialize(10);
-        Main.setGrid(new int[gridSize][gridSize]);
+        Main.setGrid(new int [gridSize][gridSize]);
         Main.robot.setDirection(1);
         Main.robot.setxPosition(0);
         outputStreamCaptor_message.reset();
@@ -201,8 +227,11 @@ class WhiteBoxTest {
         Assertions.assertEquals(0, Main.robot.getyPosition());
         Assertions.assertFalse(Main.robot.isPenUp());
         Assertions.assertEquals("", outputStreamCaptor_message.toString().trim());
+
+        //t4
+        //test case: pen down facing east move outside border
         Main.initialize(10);
-        Main.setGrid(new int[gridSize][gridSize]);
+        Main.setGrid(new int [gridSize][gridSize]);
         Main.robot.setDirection(1);
         Main.robot.setxPosition(9);
         outputStreamCaptor_message.reset();
@@ -213,8 +242,11 @@ class WhiteBoxTest {
         Assertions.assertEquals(0, Main.robot.getyPosition());
         Assertions.assertFalse(Main.robot.isPenUp());
         Assertions.assertEquals("Invalid move (out of bound)\r\n", outputStreamCaptor_message.toString());
+
+        //t5
+        //test case: pen down facing south move inside border
         Main.initialize(10);
-        Main.setGrid(new int[gridSize][gridSize]);
+        Main.setGrid(new int [gridSize][gridSize]);
         Main.robot.setDirection(2);
         Main.robot.setxPosition(0);
         outputStreamCaptor_message.reset();
@@ -225,8 +257,11 @@ class WhiteBoxTest {
         Assertions.assertEquals(4, Main.robot.getyPosition());
         Assertions.assertFalse(Main.robot.isPenUp());
         Assertions.assertEquals("", outputStreamCaptor_message.toString().trim());
+
+        //t6
+        //test case: pen down facing south move outside border
         Main.initialize(10);
-        Main.setGrid(new int[gridSize][gridSize]);
+        Main.setGrid(new int [gridSize][gridSize]);
         Main.robot.setDirection(2);
         Main.robot.setxPosition(0);
         outputStreamCaptor_message.reset();
@@ -237,8 +272,12 @@ class WhiteBoxTest {
         Assertions.assertEquals(2, Main.robot.getyPosition());
         Assertions.assertFalse(Main.robot.isPenUp());
         Assertions.assertEquals("Invalid move (out of bound)\r\n", outputStreamCaptor_message.toString());
+
+
+        //t7
+        //test case: pen down facing west move inside border
         Main.initialize(10);
-        Main.setGrid(new int[gridSize][gridSize]);
+        Main.setGrid(new int [gridSize][gridSize]);
         Main.robot.setDirection(3);
         Main.robot.setxPosition(5);
         outputStreamCaptor_message.reset();
@@ -249,8 +288,11 @@ class WhiteBoxTest {
         Assertions.assertEquals(0, Main.robot.getyPosition());
         Assertions.assertFalse(Main.robot.isPenUp());
         Assertions.assertEquals("", outputStreamCaptor_message.toString().trim());
+
+        //t8
+        //test case: pen down facing west move inside border
         Main.initialize(10);
-        Main.setGrid(new int[gridSize][gridSize]);
+        Main.setGrid(new int [gridSize][gridSize]);
         Main.robot.setDirection(3);
         Main.robot.setxPosition(3);
         outputStreamCaptor_message.reset();
@@ -261,8 +303,10 @@ class WhiteBoxTest {
         Assertions.assertEquals(0, Main.robot.getyPosition());
         Assertions.assertTrue(Main.robot.isPenUp());
         Assertions.assertEquals("Invalid move (out of bound)\r\n", outputStreamCaptor_message.toString());
+
     }
 
+    //This test function covers 100% condition, multiple condition, decision and data flow coverage for this function
     @Test
     public void testCommandInput_M() {
 
@@ -301,7 +345,7 @@ class WhiteBoxTest {
         Assertions.assertEquals(5, Main.robot.getyPosition());
         Assertions.assertEquals(0, Main.robot.getxPosition());
 
-        // 1-2-6-8-end
+        //1-2-6-8-end
         //t1
         Main.CommandInput_M("ML",gridSize,  Main.getGrid());
         Assertions.assertEquals(5, Main.robot.getyPosition());
